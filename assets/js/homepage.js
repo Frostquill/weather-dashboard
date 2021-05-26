869d6527c1ac040777426743b22933b5d9f989e2
Frostquill <ruiz82494@gmail.com>
2021-05-25 22:58:54 -0700

    search history/local Storage added

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,4 @@
+// debugger;
 var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city");
 var currentDayEl = document.querySelector("#current-container");
@@ -6,110 +7,104 @@ var fiveDayEl = document.querySelector("#five-day-forecast");
 var currentCitySearch = document.querySelector("#city-search-term");
 var cityContainer = document.querySelector("#city-container");
 var tempHistory = document.querySelector("#searchHistory");
+var btnSearch = document.querySelector("#btnSearch");
+
 var day = moment().format("MM/DD/YYYY");
-var searchHistory = [];
-
-
-
-
-
-
+var recentHistory = [];
+var dupli = false;
 
 // fetch weather api for selected city
-var getWeatherApi= function(city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + 
-    city + 
-    "&units=imperial" +
-    
-    "&appid=44780b34048acb040f5cd25cd5d97105";
-    
-    fetch(apiUrl)
-    .then(function(response){
-       if( response.ok){
-           response.json().then(function(data) {
-        displayWeather(data, city);
-       
-      }); 
-    } else {
-        alert("City does not exist.");
-      }
+var getWeatherApi = function () {
+    var city = cityInputEl.value.trim();
+    fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=imperial" +
+            "&appid=44780b34048acb040f5cd25cd5d97105"
+    ).then(
+        function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return alert("City does not exist.");
+            }
+        })
+            
 
-})
+    .then(function (response) {
+        currentDayEl.textContent = "";
+            currentCitySearch.textContent = city + ":" + "(" + day + ")";
+            cityContainer.classList = "list-group col-12 col-md-8";
+            var temp = document.createElement("p");
+            temp.setAttribute("class", "weather-details");
+            temp.textContent = "Temp:" + response.main.temp + " F";
+            currentDayEl.appendChild(temp);
+
+            var wind = document.createElement("p");
+            wind.setAttribute("class", "weather-details");
+            wind.textContent = "Wind:" + response.wind.speed + " MPH";
+            currentDayEl.appendChild(wind);
+
+            var humidity = document.createElement("p");
+            humidity.setAttribute("class", "weather-details");
+            humidity.textContent = "Humidity:" + response.main.humidity + "%";
+            currentDayEl.appendChild(humidity);
+            createBtn(city);
+        // console.log(response);
+        // saveSearch(city);
+
+});
 }
 
-var loadSearch = function() {
-    var sites = JSON.parse(localStorage.getItem('cities'));
-    sites.forEach(createBtn);
+
+ function loadSearch(){
+    var loS = JSON.parse(localStorage.getItem('cities'));
+     if(loS === null) 
+     {loS= [] }
+    loS.forEach(function(data) {
+        createBtn(data);
     
+    });
 }
-// create btn
-var createBtn = function(recent) {
-    var btn = document.createElement("btn");
+
+var createBtn = function (recent) {
+    noDupli(recent);
+    if(!dupli) {
+    var btn = document.createElement("button");
     btn.innerText = recent;
-    btn.classList = "btn btn-primary btn-w";
+    btn.classList = "recent-btn btn btn-primary btn-w";
     tempHistory.appendChild(btn);
-    console.log(recent);
+    recentHistory.push(recent);
+    // console.log(saveData);
+    localStorage.setItem('cities', JSON.stringify(recentHistory));
+    // console.log(recent);
+    return;
+    } 
 }
 
 
 
-var saveSearch = function (saveData) {
-    searchHistory.push(saveData);
-    console.log(searchHistory);
-    localStorage.setItem('cities', JSON.stringify(searchHistory));
-}
-
-//Search Bar input
-var citySearch = function(event) {
-event.preventDefault();
-var citySelect = cityInputEl.value.trim();
-// console.log(citySelect);
-if (citySelect) {
-    getWeatherApi(citySelect);
-    createBtn(citySelect);
-    saveSearch(citySelect);
-    cityInputEl.textContent = "";
+var noDupli = function(city) {
     
-} else {
-    alert("Please enter a city");
-    // console.log(event);
+    for (var i = 0; i < recentHistory.length; i++ ) {
+        if(city === recentHistory[i]) {
+            return (dupli = true);
+        } else {
+            dupli = false;
+        }
+    }
 }
-};
-
-var displayWeather = function (cities , searchTerm ) {
-    currentDayEl.textContent = "";
-    currentCitySearch.textContent = searchTerm + ":" +"(" + day + ")";
-    cityContainer.classList = "list-group col-12 col-md-8";
-    var temp = document.createElement("p");
-    temp.setAttribute("class", "weather-details");
-    temp.textContent = "Temp:" + cities.main.temp + " F";
-    currentDayEl.appendChild(temp);
-
-    var wind = document.createElement("p");
-    wind.setAttribute("class", "weather-details");
-    wind.textContent = "Wind:" + cities.wind.speed + " MPH";
-    currentDayEl.appendChild(wind);
-
-    var humidity = document.createElement("p");
-    humidity.setAttribute("class", "weather-details");
-    humidity.textContent = "Humidity:" + cities.main.humidity + "%";
-    currentDayEl.appendChild(humidity);
-    // console.log(cities);
-
-}
-
-var recentClick = function() {
-    
-}
-
-
-
-
-
-
-
-
-
 
 loadSearch();
-cityFormEl.addEventListener("submit", citySearch);
+console.log(recentBtn);
+var recentBtn = document.querySelectorAll(".recent-btn");
+recentBtn.forEach(function(btn){
+    btn.addEventListener("click", function(){
+        var btnVal = this.textContent;
+        console.log(btnVal);
+        cityInputEl.value = btnVal; 
+    })
+})
+btnSearch.addEventListener("click", function () {
+    getWeatherApi();
+});
